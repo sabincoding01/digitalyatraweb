656098b6ac6eb9f916eceae0c362372c724a3285
sabincoding01 <sabintimalsina74@gmail.com>
2026-03-09 22:23:45 +0545

    store final

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,35 @@ import Login from "./Auth/Login";
 import Register from "./Auth/Register";
 import AddCourses from "./AddComponent/AddCourses";
 import { TopBarProvider } from "./context/TopBarContext";
+import { Provider } from "react-redux";
+import store from "../store/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Auth pages - NavBar/TopBar छैन */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          {/* Auth pages - NavBar/TopBar छैन */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-        {/* Main pages - Layout (NavBar + TopBar) सहित */}
-        <Route
-          path="/*"
-          element={
-            <TopBarProvider>
-              <Layout />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<h1>About</h1>} />
-
-                <Route path="/add-course" element={<AddCourses />} />
-              </Routes>
-            </TopBarProvider>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+          {/* Main pages - Layout (NavBar + TopBar) सहित */}
+          <Route
+            path="/*"
+            element={
+              <TopBarProvider>
+                <Layout />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<h1>About</h1>} />
+                  <Route path="/add-course" element={<AddCourses />} />
+                </Routes>
+              </TopBarProvider>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
