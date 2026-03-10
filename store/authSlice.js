fc8838cd762fa8b5b948ad5f5264a4bf94456202
sabincoding01 <sabintimalsina74@gmail.com>
2026-03-10 16:25:53 +0545

    status

--- store/authSlice.js
+++ store/authSlice.js
@@ -1,65 +1,76 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
+import STATUSES from "../src/global/statuses";
 
-const authSlice= createSlice({
-name:"auth",
+const authSlice = createSlice({
+  name: "auth",
 
-initialState:{
-
-  user:null,
-  email:null,
-  token:null,
-  status:null,
-},
-
-reducers:{
-  setStatus(state,action){
-    state.status = action.payload
+  initialState: {
+    user: null,
+    email: null,
+    token: null,
+    status: null,
   },
 
-  setUser(state,action){
-    state.user= action.payload.user
+  reducers: {
+    setStatus(state, action) {
+      state.status = action.payload;
+    },
+
+    setUser(state, action) {
+      state.user = action.payload;
+    },
+
+    setToken(state, action) {
+      state.token = action.payload;
+    },
+
+    setEmail(state, action) {
+      state.email = action.payload;
+    },
   },
+});
 
-  setToken(state,action){
-    state.token=action.payload.token
-  },
+export const { setUser, setToken, setStatus, setEmail } = authSlice.actions;
+export default authSlice.reducer;
 
-  setEmail(state,action){
-    state.email=action.payload.email
-  }
-  }
+function register(date) {
+  return async function registerThunk(dispatch) {
+    dispatch(setUser(data));
+    dispatch(setStatus(STATUSES.LOADING));
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/register",
+        date,
+      );
 
-  })
-
-  export const {setUser,setToken,setStatus,setEmail} = authSlice.actions
-  export default authSlice.reducer
-
-   function register(date){
-  return async function registerThunk(dispatch){
-    const response = await axios.post("http://localhost:3000/api/auth/register",date);
-    
-    if(response === 200){
-
-    }
-
-    else{
-
-      
-    }
-    
-  }
-  }
-
-  function login(data){
-    return async function login(dispatch){
-      const response = await axios.post("http://localhost:3000/api/auth/login",data);
-      
-      if(response === 200){
-
+      if (response === 200) {
+        dispatch(setStatus(STATUSES.SUCCESS));
+      } else {
+        dispatch(setStatus(STATUSES.ERROR));
       }
-
-      else{
-        
-      }
+    } catch (error) {
+      dispatch(setStatus(STATUSES.ERROR));
     }
-  }
+  };
+}
+
+function login(data) {
+  return async function login(dispatch) {
+    dispatch(setUser(data));
+    dispatch(setStatus(STATUSES.Loading));
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/login",
+        data,
+      );
+
+      if (response === 200) {
+        dispatch(setSuccess(STATUSES.LOADING));
+      } else {
+        dispatch(setStatus(STATUSES.ERROR));
+      }
+    } catch (error) {
+      dispatch(setStatus(STATUSES.ERROR));
+    }
+  };
+}
