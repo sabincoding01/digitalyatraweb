8335b99acedfbbb9dd6bd355d22bfd7b963cda69
sabincoding01 <sabintimalsina74@gmail.com>
2026-03-10 15:13:59 +0545

    authThunk

--- store/authSlice.js
+++ store/authSlice.js
@@ -1,26 +1,65 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
-const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    user: null,
-    email: null,
-    token: null,
-    status: null,
+const authSlice= createSlice({
+name:"auth",
+
+initialState:{
+
+  user:null,
+  email:null,
+  token:null,
+  status:null,
+},
+
+reducers:{
+  setStatus(state,action){
+    state.status = action.payload
   },
 
-  reducers: {
-    setStatus: (state, action) => {
-      state.status = action.payload;
-    },
-    setUser: (state, action) => {
-      state.user = action.payload.user;
-    },
-    setToken: (state, action) => {
-      state.token = action.payload.token;
-    },
+  setUser(state,action){
+    state.user= action.payload.user
   },
-});
 
-export const { setStatus, setUser, setToken } = authSlice.actions;
-export default authSlice.reducer;
+  setToken(state,action){
+    state.token=action.payload.token
+  },
+
+  setEmail(state,action){
+    state.email=action.payload.email
+  }
+  }
+
+  })
+
+  export const {setUser,setToken,setStatus,setEmail} = authSlice.actions
+  export default authSlice.reducer
+
+   function register(date){
+  return async function registerThunk(dispatch){
+    const response = await axios.post("http://localhost:3000/api/auth/register",date);
+    
+    if(response === 200){
+
+    }
+
+    else{
+
+      
+    }
+    
+  }
+  }
+
+  function login(data){
+    return async function login(dispatch){
+      const response = await axios.post("http://localhost:3000/api/auth/login",data);
+      
+      if(response === 200){
+
+      }
+
+      else{
+        
+      }
+    }
+  }
